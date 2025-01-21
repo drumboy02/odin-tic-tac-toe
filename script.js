2453caf3a50901905681840c23cd96bce360741d
drumboy02 <153551032+drumboy02@users.noreply.github.com>
2025-01-21 14:50:56 -0500

    created factory functions for createBoard and createPlayer

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-const h1 = document.querySelector('h1');
-h1.style.color = 'green';
+// Store gameboard array in object
+const createBoard = (() => {
+    // Initialize private variables
+    const board = [];
+    for (i = 0; i < 9; i++) board.push(null);
+    // Define methods
+    const getBoard = () => board;
+    // Return methods
+    return { getBoard };
+})()
+
+// Store players in objects
+const createPlayer = (playerName, playerMark) => {
+    // Initialize private variables
+    const name = playerName;
+    const mark = playerMark;
+    let score = 0;
+    // Define methods
+    const getName = () => name;
+    const getScore = () => score;
+    const incScore = () => { score++ };
+    const makeMark = () => mark;
+    // Return methods
+    return { getName, getScore, incScore, makeMark };
+}
+
+const player1 = createPlayer('Brett1', 'X');
+const player2 = createPlayer('Brett2', 'O');
+const gameboard = createBoard;

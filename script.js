c0caffffa5f1fa96896538305eb9e890e2ad2703
drumboy02 <153551032+drumboy02@users.noreply.github.com>
2025-01-22 03:02:32 -0500

    added check for null value to each conditional expression

--- script.js
+++ script.js
@@ -48,33 +48,28 @@ const gameboard = createBoard;
 
 // fill the board with marks
 for (let i = 0, len = gameboard.getBoard().length; i < len; i++) {
-    if (!(i % 2)) {
-        gameboard.markSquare(i, player1.makeMark());
-    } else {
-        gameboard.markSquare(i, player2.makeMark());
-    }
-    gameboard.markSquare(0, player2.makeMark())
+    gameboard.markSquare(i, player1.makeMark());
 }
 
 let test = gameboard.getBoard();
-if (test[0] === test[1] && test[1] === test[2]) {
+if (test[0] !== null && test[0] === test[1] && test[1] === test[2]) {
     // test rows
     console.log('win 1-3');
-} else if (test[3] === test[4] && test[4] === test[5]) {
+} else if (test[3] !== null && test[3] === test[4] && test[4] === test[5]) {
     console.log('win 4-6');
-} else if (test[6] === test[7] && test[7] === test[8]) {
+} else if (test[6] !== null && test[6] === test[7] && test[7] === test[8]) {
     console.log('win 7-9');
-} else if (test[0] === test[3] && test[3] === test[6]) {
+} else if (test[0] !== null && test[0] === test[3] && test[3] === test[6]) {
     // test columns
     console.log('win 1,4,7');
-} else if (test[1] === test[4] && test[4] === test[7]) {
+} else if (test[1] !== null && test[1] === test[4] && test[4] === test[7]) {
     console.log('win 2,5,8');
-} else if (test[2] === test[5] && test[5] === test[8]) {
+} else if (test[2] !== null && test[2] === test[5] && test[5] === test[8]) {
     console.log('win 3,6,9');
-} else if (test[0] === test[4] && test[4] === test[8]) {
+} else if (test[0] !== null && test[0] === test[4] && test[4] === test[8]) {
     // test diagonals
     console.log('win 1,5,9');
-} else if (test[2] === test[4] && test[4] === test[6]) {
+} else if (test[2] !== null && test[2] === test[4] && test[4] === test[6]) {
     console.log('win 3,5,7');
 }
 

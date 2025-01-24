8d8568e7971ab53f0c377086d5d6288b1e53be4f
drumboy02 <153551032+drumboy02@users.noreply.github.com>
2025-01-24 00:49:39 -0500

    added some comments to code

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ const createBoard = (() => {
     }
 
     // Initialize gameboard
-    initBoard();    
+    initBoard();
+
     return { initBoard, getBoard, markSquare };
 })()
 
@@ -45,9 +46,10 @@ const playGame = () => {
     let round = 1;
     let playerTurn = `${player1.getName()}'s turn`;
 
-    // define methods 
+    // Define methods 
     const board = (() => { return gameboard })();
     const getPlayer = () => {
+        // Player1's turn? else player2
         if (getTurn() === `${player1.getName()}'s turn`) {
             return player1;
         } else {
@@ -59,6 +61,7 @@ const playGame = () => {
         if (getTurn() === `${player1.getName()}'s turn`) {
             playerTurn = `${player2.getName()}'s turn`;
         } else {
+            // Round is over 
             playerTurn = `${player1.getName()}'s turn`;
             incRound();
         }
@@ -71,36 +74,43 @@ const playGame = () => {
         let player = getPlayer();
 
         if (test[0] !== null && test[0] === test[1] && test[1] === test[2]) {
-            // test rows
+            // Test rows
             console.log(`${player.getName()} wins 1-3`);
             player.incScore();
             console.log(`${player.getName()}'s score: ${player.getScore()}`);
+            // board.initBoard();
         } else if (test[3] !== null && test[3] === test[4] && test[4] === test[5]) {
             console.log(`${player.getName()} wins 4-6`);
             player.incScore();
             console.log(`${player.getName()}'s score: ${player.getScore()}`);
+            // board.initBoard();
         } else if (test[6] !== null && test[6] === test[7] && test[7] === test[8]) {
             console.log(`${player.getName()} wins 7-9`);
             player.incScore();
             console.log(`${player.getName()}'s score: ${player.getScore()}`);
+            // board.initBoard();
         } else if (test[0] !== null && test[0] === test[3] && test[3] === test[6]) {
-            // test columns
+            // Test columns
             console.log(`${player.getName()} wins 1,4,7`);
             player.incScore();
             console.log(`${player.getName()}'s score: ${player.getScore()}`);
+            // board.initBoard();
         } else if (test[1] !== null && test[1] === test[4] && test[4] === test[7]) {
             console.log(`${player.getName()} wins 2,5,8`);
             player.incScore();
             console.log(`${player.getName()}'s score: ${player.getScore()}`);
+            // board.initBoard();
         } else if (test[2] !== null && test[2] === test[5] && test[5] === test[8]) {
             console.log(`${player.getName()} wins 3,6,9`);
             player.incScore();
             console.log(`${player.getName()}'s score: ${player.getScore()}`);
+            // board.initBoard();
         } else if (test[0] !== null && test[0] === test[4] && test[4] === test[8]) {
-            // test diagonals
+            // Test diagonals
             console.log(`${player.getName()} wins 1,5,9`);
             player.incScore();
             console.log(`${player.getName()}'s score: ${player.getScore()}`);
+            // board.initBoard();
         } else if (test[2] !== null && test[2] === test[4] && test[4] === test[6]) {
             console.log(`${player.getName()} wins 3,5,7`);
             player.incScore();
@@ -137,50 +147,48 @@ const game = playGame();
 
 // round 1
 console.log(`round: ${game.getRound()}`);
-console.log(game.getTurn())
 
+console.log(game.getTurn())
 game.board.markSquare(0, game.getPlayer().makeMark())
 game.checkWin();
 
 console.log(game.getTurn())
-
 game.board.markSquare(1, game.getPlayer().makeMark())
 game.checkWin();
-game.log()
 
+game.log()
 
 // round 2
 console.log(`round: ${game.getRound()}`);
-console.log(game.getTurn())
 
+console.log(game.getTurn())
 game.board.markSquare(2, game.getPlayer().makeMark())
 game.checkWin();
 
 console.log(game.getTurn())
-
 game.board.markSquare(3, game.getPlayer().makeMark())
 game.checkWin();
-game.log()
 
+game.log()
 
 // round 3
 console.log(`round: ${game.getRound()}`);
-console.log(game.getTurn())
 
+console.log(game.getTurn())
 game.board.markSquare(4, game.getPlayer().makeMark())
 game.checkWin();
 
 console.log(game.getTurn())
-
 game.board.markSquare(5, game.getPlayer().makeMark())
 game.checkWin();
-game.log()
 
+game.log()
 
 // round 4
 console.log(`round: ${game.getRound()}`);
-console.log(game.getTurn())
 
+console.log(game.getTurn())
 game.board.markSquare(6, game.getPlayer().makeMark())
 game.checkWin();
+
 game.log()
